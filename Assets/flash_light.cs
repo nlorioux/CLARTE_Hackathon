60055054a00eb59cddaf01f0de7a65ebc902bdec
noe-morel <120017007+noe-morel@users.noreply.github.com>
2023-01-27 09:37:01 +0100

    commit état actuel de la branche

---
+++ Assets/flash_light.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flash_light : MonoBehaviour
+{
+    public GameObject left_controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        left_controller = GameObject.Find("LeftHandAnchor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = left_controller.transform.position;
+    }
+}
